b4a02c4db032aaa3777300a8a547a3b2857a0b0e
rephlekt <rephlekt2718@gmail.com>
2023-02-22 11:14:06 -0600

    fixed __dirname path issue

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import path, { dirname } from "path"
+import path from "path"
+import { fileURLToPath } from "url"
 import express from "express"
 import hbs from "hbs"
 import geocode from './utils/geocode.js'
@@ -7,11 +8,12 @@ import forecast from './utils/forecast.js'
 const app = express()
 const port = process.env.PORT || 3000
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(
+    import.meta.url)
+const __dirname = path.dirname(__filename)
 const publicDirPath = path.join(__dirname, '../public/')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
